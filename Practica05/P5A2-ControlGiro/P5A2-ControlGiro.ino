e8a2cd7f7dbd127616890e6e99f1045219831bdc
DiegoHuesos <diegohd_98@hotmail.com>
2022-09-29 18:48:54 -0500

    Control Giro

--- Practica05/P5A2-ControlGiro/P5A2-ControlGiro.ino
+++ Practica05/P5A2-ControlGiro/P5A2-ControlGiro.ino
@@ -1,53 +1,55 @@
+#include <Wire.h>
 #define MotA 10
 #define MotB 11
-#define pot A0
+#define pot A10
+#define sal 9
 
-int value = 0;
+float pote, potencia, salida;
 
 void setup() {
-  pinMode(pot, INPUT);
-  pinMode(MotA, OUTPUT);
-  pinMode(MotB, OUTPUT);
+  pinMode(MotA,OUTPUT);
+  pinMode(MotB,OUTPUT);
+  pinMode(sal, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  value = analogRead(pot);
-  Serial.print("Pot: ");
-  Serial.print(value);
-  Serial.print("   --> ");
+  pote=analogRead(pot);
+  potencia = map(pote,0, 1023, 0, 5);
   
-  if(value < 409.2){
-    levogiro();
-  }
-
-  if(409.2 < value and value < 613.8){
-    detenido();
-  }
-  
-  if(613.8 < value and value < 1023){
-    dextrogiro();
-  }
-  
+  if(potencia>=3 && potencia<= 5){
+    Dextrogiro();
+    salida = map(pote,615,1023,0,255);
+    }else{
+      if(potencia>=2){
+        salida = 0;
+        Detenido();
+        }else{
+          salida = map(pote,410,0,0,255);
+          Levogiro();
+        }
+      }
+   
+   analogWrite(sal, salida);
+   Serial.println(salida);
+   Serial.println(potencia);
 }
 
-void levogiro(){
-  digitalWrite(MotA, 1);
-  digitalWrite(MotB, 0);
-  Serial.println("Levogiro");
-  delay(1000);
+void Levogiro(){
+  digitalWrite(MotA, HIGH);
+  digitalWrite(MotB, LOW);
+  Serial.print("Levogiro ");
 }
 
-void dextrogiro(){
-  digitalWrite(MotA, 0);
-  digitalWrite(MotB, 1);
-  Serial.println("Dextrogiro");
-  delay(1000);
+void Detenido(){
+  digitalWrite(MotA, LOW);
+  digitalWrite(MotB, LOW);
+  Serial.print("Detenido");
 }
 
-void detenido(){
-  digitalWrite(MotA, 1);
-  digitalWrite(MotB, 1);
-  Serial.println("Detenido");
-  delay(1000);
+void Dextrogiro(){
+  digitalWrite(MotA, LOW);
+  digitalWrite(MotB, HIGH);
+  Serial.print("Dextrogiro ");
+  delay(253);
 }
